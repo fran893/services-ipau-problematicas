f5aabd4645978f23aab42f790a3cd83eb4b9e22b
Franco <alvarez.fr893@gmail.com>
2020-02-18 14:46:16 -0300

    Se cambio la manera de llamar a los endpoints.

--- services-ipau-problematicas/src/main/java/com/app/ipau/problematicas/models/dao/ProblematicaDao.java
+++ services-ipau-problematicas/src/main/java/com/app/ipau/problematicas/models/dao/ProblematicaDao.java
@@ -1,11 +1,9 @@
 package com.app.ipau.problematicas.models.dao;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.CrudRepository;
 
 import com.app.ipau.commons.problematicas.models.entities.Problematica1;
 
-@RepositoryRestResource(path = "problematicas1")
-public interface ProblematicaDao extends PagingAndSortingRepository <Problematica1, Integer> {
+public interface ProblematicaDao extends CrudRepository <Problematica1, Integer> {
 
 }
---
+++ services-ipau-problematicas/src/main/java/com/app/ipau/problematicas/models/services/IProblematicas.java
@@ -0,0 +1,11 @@
+package com.app.ipau.problematicas.models.services;
+
+import java.util.List;
+
+import com.app.ipau.commons.problematicas.models.entities.Problematica1;
+
+public interface IProblematicas {
+	
+	public List<Problematica1> findAll();
+
+}
